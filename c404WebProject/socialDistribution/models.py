6f1dca02697b14f6200ef2cfa78ee8dcb3cccf40
XUPINGI <xuping@ualberta.ca>
2016-10-07 12:36:47 -0600

    Finished first draft of post model without image field.

--- c404WebProject/socialDistribution/models.py
+++ c404WebProject/socialDistribution/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+import uuid
+
 # Create your models here.
+
+class Post(models.Model):
+    idKey=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    title=models.CharField(max_length=500)
+    content=models.TextField()
+    
+    author=models.ForeignKey(Author,on_delete=models.CASCADE)
+    
+    public="1"
+    local="2"
+    foaf="3"
+    localfriends="4"
+    list_of_friends="5"
+    private="6"
+    
+    visuability_choice=(
+        (public,'Public'),
+        (local,'Public to local'),
+        (foaf,'Friends of friends'),
+        (local_friends,'Local friends'),
+        (list_of_friends,'friend selected'),
+        (private,'Myself only')
+    )
+    visuability=models.CharField(max_length=1,choices=visuability_choice,default=public)
+    
+    list_of_friends_can_view=models.ManyToManyField(Author)
+    
+    publish_time=models.DateTimeField(auto_now=True)
+    
+    
+                
