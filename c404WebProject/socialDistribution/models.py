6f7a76cf914f6893174eb2f8bf9dca6a1b97d0da
XUPINGI <xuping@ualberta.ca>
2016-10-07 15:30:12 -0600

    Finished draft model for comment and author.

--- c404WebProject/socialDistribution/models.py
+++ c404WebProject/socialDistribution/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     idKey=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     title=models.CharField(max_length=500)
     content=models.TextField()
-    
+    tag=models.ForeignKey(Tag,on_delete=models.CASCADE)
     author=models.ForeignKey(Author,on_delete=models.CASCADE)
     
     public="1"
@@ -41,17 +41,31 @@ class Author(models.Model):
     idKey=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     first_name=models.CharField(max_length=20)
     last_name=models.CharField(max_length=20)
-    
+    githubName=models.CharField(max_length=35)
+    bio=models.TextField()
     email=models.EmailField()
-    password
-    
     url=model.URLField()
-    
     friends=models.ManyToManyField(Author)
+    
+    def get_idKey(self):
+        return self.idKey
+    
+    def get_name(self):
+        return self.first_name+" "+self.last_name
+    
+    def get_url(self):
+        return self.url
                 
 
 class Comment(models.Model):
     idKey=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
-    
     post=models.ForeignKey(Post,on_delete=models.CASCADE)
+    author=models.ForeignKey(Author,on_delete=models.CASCADE)
+    publish_time=models.DateTimeField(auto_now=True)
+
+class TagForPost(models.Model):
+    tag=models.CharField(max_Length=20)
+    
+    def __str__(self):
+        return self.tag
     
