13dec476e507a7a1ca681c08caf2941de36709e0
XUPINGI <xuping@ualberta.ca>
2016-10-13 12:34:18 -0600

    add some admin registration.

--- c404WebProject/socialDistribution/admin.py
+++ c404WebProject/socialDistribution/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from models import Author, Comment, Post
 # Register your models here.
+
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Author)
+
+admin.site.unregister(Author)
