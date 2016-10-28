284d0f5ab8bb47545c842bb886c1d2ccb22308d2
jihwan <jihwan@ualberta.ca>
2016-10-28 16:15:13 -0600

    posts related model, serializer, views changed continuing work on post

--- c404WebProject/web_api/serializers.py
+++ c404WebProject/web_api/serializers.py
@@ -17,7 +17,7 @@ class UserSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', 'title', 'source', 'origin', 'description', 'content', 'tag', 'author', 'visibility', 'publish_time')
+        fields = ('count', 'id', 'title', 'source', 'origin', 'description', 'content', 'category', 'author', 'visibility', 'publish_time')
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
