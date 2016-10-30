8c5f59442e018bcad754e81ebdc1baf45e7453d2
XUPINGI <xuping@ualberta.ca>
2016-10-30 14:09:56 -0600

    Implemented accept request.

--- web_api/tests/tests.py
+++ web_api/tests/tests.py
@@ -106,23 +106,40 @@ class FriendRequestTestCase(APITestCase):
 
 	self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response)
     
-
-'''
     def test_response_request(self):
 	self.sender = createAuthor(self,0)
-	self.receiver = createAuthor(self,1)	
+	self.receiver = createAuthor(self,1)
+
+
+	self.client.post('/friendrequest/', {
+	    "query" : "friendrequest",
+	    "author" : {
+	        "id" : self.sender.id,
+	        "host" : self.sender.host,
+	        "displayName" : self.sender.user.username,
+	    },	
+	    "friend": {
+	        "id" : self.receiver.id,
+	        "host" : self.receiver.host,
+	        "displayName" : self.receiver.user.username,
+	        "url" : self.receiver.host+"author/"+str(self.receiver.id)
+	    }
+	}, format='json')
 		
 	response = self.client.post('/friendrequest/', {
-	    "query" : "friendrequest",
+	    "query" : "friendresponse",
 	    "author" : {
 	        "id" : self.sender.id
 	    },	
 	    "friend": {
 	        "id" : self.receiver.id
 	    },
-	    "accepted" : true
+	    "accepted" : 'true'
 	}, format='json')
-    '''
+
+	self.assertEqual(response.status_code, status.HTTP_200_OK, response)
+	self.assertTrue(Author.objects.get(id=self.receiver.id).friends.all().filter(id=self.sender.id).exists())
+	self.assertTrue(Author.objects.get(id=self.sender.id).friends.all().filter(id=self.receiver.id).exists())
 	
 
 class FriendServiceTestCase(APITestCase):
