a88c3985a88a5876bc5ddf4e356f52309a6d7b84
Alice Wu <mingxun@ualberta.ca>
2016-10-30 18:38:54 -0600

    removed unused dependencies and file

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 echo Building API
 git remote add api https://git.heroku.com/api-bloggyblog404.git
-git pull api master
 heroku buildpacks:set heroku/python --remote api
 git commit --allow-empty -m "Deploying api"
-git push api master
+git -f push api master
 heroku run python manage.py migrate --remote api
 
 echo Building Client
 npm install
 npm run build-prod
 git remote add client https://git.heroku.com/bloggyblog404.git
-git pull client master
 heroku buildpacks:set heroku/nodejs --remote client
 git add client
 git commit --allow-empty -m "Deploying Client"
-git push client master
+git push -f client master
 
 heroku ps:scale web=1 --app bloggyblog404
 heroku ps:scale web=1 --app api-bloggyblog404
