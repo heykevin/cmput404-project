3b8d956b94bb959b794d7775387f2d0e4f77bf64
Kevin Tang <tkevin@ualberta.ca>
2016-11-01 02:26:24 -0600

    Deploying commits more better named

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo Building API
 git remote add api https://git.heroku.com/bloggyblog404.git
 heroku buildpacks:set heroku/python --remote api
-git commit --allow-empty -m "Deploying"
+git commit --allow-empty -m "Deploying api"
 git push api master
 heroku run python manage.py migrate --remote api
 
@@ -12,7 +12,7 @@ npm build-prod
 git remote add client https://git.heroku.com/api-bloggyblog404.git
 heroku buildpacks:set heroku/nodejs --remote client
 git add client
-git commit --allow-empty -m "Deploying"
+git commit --allow-empty -m "Deploying Client"
 git push client master
 
 heroku ps:scale web=1 --app bloggyblog404
