6c7af2a6984cd8008d51c3b1b28c3e609cda57ca
Kevin Tang <tkevin@ualberta.ca>
2016-11-01 02:21:08 -0600

    Modifying deploy to add correct buildpacks

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo Building API
 git remote add api https://git.heroku.com/bloggyblog404.git
+heroku buildpacks:set heroku/python --remote api
 git commit --allow-empty -m "Deploying"
 git push api master
 heroku run python manage.py migrate --remote api
@@ -9,6 +10,7 @@ echo Building Client
 npm install
 npm build-prod
 git remote add client https://git.heroku.com/api-bloggyblog404.git
+heroku buildpacks:set heroku/nodejs --remote client
 git add client
 git commit --allow-empty -m "Deploying"
 git push client master
