530ca8995fb789be57cb58ee0b1182d2d91c4f5e
Kevin Tang <tkevin@ualberta.ca>
2016-11-01 02:06:58 -0600

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Building API
-git remote add api https://git.heroku.com/gentle-hollows-68494.git
+git remote add api https://git.heroku.com/bloggyblog404.git
 git commit --allow-empty -m "Deploying"
 git push api master
 heroku run python manage.py migrate --remote api
@@ -8,7 +8,10 @@ heroku run python manage.py migrate --remote api
 echo Building Client
 npm install
 npm build-prod
-git remote add client https://git.heroku.com/eerie-mansion-58762.git
+git remote add client https://git.heroku.com/api-bloggyblog404.git
 git add client
 git commit --allow-empty -m "Deploying"
 git push client master
+
+heroku ps:scale web=1 --app bloggyblog404
+heroku ps:scale web=1 --app api-bloggyblog404
