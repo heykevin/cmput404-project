5cdb4ac61c4cbd06f44b43a3e435c40f4c3458b6
Kevin Tang <tkevin@ualberta.ca>
2016-11-01 01:03:35 -0600

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo Building API
+git remote add api https://gentle-hollows-68494.herokuapp.com/
+git commit --allow-empty -m "Deploying"
+git push api master
+
+echo Building Client
+git remote add client https://eerie-mansion-58762.herokuapp.com/
+git commit --allow-empty -m "Deploying"
+git push client master
