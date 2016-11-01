806d93e48eb65aba083db6ec59924fa5ab0350de
Kevin Tang <tkevin@ualberta.ca>
2016-11-01 03:59:00 -0600

    debugging scripts

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,16 @@ echo Building API
 git remote add api https://git.heroku.com/api-bloggyblog404.git
 heroku buildpacks:set heroku/python --remote api
 git commit --allow-empty -m "Deploying api"
-git -f push api master
+git --force push api master
 heroku run python manage.py migrate --remote api
 
 echo Building Client
-npm install
-npm run build-prod
+echo $environment
 git remote add client https://git.heroku.com/bloggyblog404.git
 heroku buildpacks:set heroku/nodejs --remote client
 git add client
 git commit --allow-empty -m "Deploying Client"
-git push -f client master
+git push --force client master
 
 heroku ps:scale web=1 --app bloggyblog404
 heroku ps:scale web=1 --app api-bloggyblog404
--- heroku.sh
+++ heroku.sh
@@ -1,5 +1,8 @@
+echo $NODE_ENV
 if ["$NODE_ENV" == "true"]; then
+    echo "Starting node server"
     node client/server.js
 else
+    echo "starting django app"
     gunicorn c404WebProject.wsgi --log-file -
 fi
