a5da0ff69c225d51c06610fd5dde33763636ef07
Kevin Tang <tkevin@ualberta.ca>
2016-11-01 01:21:08 -0600

    WHAT IS GOING ON

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo Building API
 git remote add api https://gentle-hollows-68494.herokuapp.com/
 git commit --allow-empty -m "Deploying"
 git push api master
-heroku run python manage.py migrate
+heroku run python manage.py migrate --remote client
 
 echo Building Client
 git remote add client https://eerie-mansion-58762.herokuapp.com/
