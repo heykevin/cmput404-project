e6c1a0f6c80ebd796ea863c589ae46a21660c1a9
Kevin Tang <tkevin@ualberta.ca>
2016-11-01 01:24:42 -0600

    fixing migrate

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo Building API
 git remote add api https://gentle-hollows-68494.herokuapp.com/
 git commit --allow-empty -m "Deploying"
 git push api master
-heroku run python manage.py migrate --remote client
+heroku run python manage.py migrate --remote api
 
 echo Building Client
 git remote add client https://eerie-mansion-58762.herokuapp.com/
