c832e9f4370a9b6c04ca815a66f4ea6516c7008b
Kevin Tang <tkevin@ualberta.ca>
2016-11-01 04:10:59 -0600

    Okay its working I think?

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-echo Building API
+echo Building API ------------------------------
 git remote add api https://git.heroku.com/api-bloggyblog404.git
 heroku buildpacks:set heroku/python --remote api
 git commit --allow-empty -m "Deploying api"
 git push --force api master
 heroku run python manage.py migrate --remote api
 
-echo Building Client
-echo $environment
+echo Building Client -----------------------------
+npm install
+npm run build-prod
 git remote add client https://git.heroku.com/bloggyblog404.git
 heroku buildpacks:set heroku/nodejs --remote client
 git add client
