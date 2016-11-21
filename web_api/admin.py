9ada266d775a0b991ca54d0608ce83c160014448
Alice Wu <mingxun@ualberta.ca>
2016-11-21 09:46:04 -0700

    allow admin access for modifying node access in admin page

--- web_api/admin.py
+++ web_api/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Author, Comment, Post
+from .models import Author, Comment, Post, Image, Node
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Author)
+admin.site.register(Image)
+admin.site.register(Node)
