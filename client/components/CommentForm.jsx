63658a99d22561a078c9a41c7ae45604c3afb9a9
Phil <phlam1@ualberta.ca>
2016-11-28 16:15:33 -0700

    commit for pull

--- client/components/CommentForm.jsx
+++ client/components/CommentForm.jsx
@@ -36,7 +36,7 @@ export class CommentForm extends React.Component
             type: "commentsAddComment",
             content: this.state.content,
             postId: this.props.postId,
-            contentType: this.state.contentType
+            contentType: "text/x-markdown"
         });
         this.setState({content: ''});
     }
@@ -49,19 +49,6 @@ export class CommentForm extends React.Component
         });
     }
 
-    getEditorMode() {
-        return this.state.editorModeOverride ? this.state.contentType === "text/markdown" : (this.state.contentType && this.state.contentType ? this.state.contentType ==="text/markdown" : true);
-    }
-
-    handleSelect(isMarkdownContent)
-    {
-        this.setState({
-            editorModeOverride: true,
-            contentType: isMarkdownContent ? "text/markdown" : "text/plain",
-            edited: true
-        });
-    }
-
     isButtonDisabled() {
         return !(this.state.content.length > 0);
     }
@@ -71,31 +58,14 @@ export class CommentForm extends React.Component
         return (
             <div className = "comment-form">
                 <h4>Write Some Comments</h4>
-                    <Nav bsStyle="pills" onSelect={this.handleSelect}>
-                        <NavItem className="nav-item float-right" eventKey={false}>
-                            SimpleText
-                        </NavItem>
-                        <NavItem className="nav-item float-right" eventKey={true}>
-                            MarkDown
-                        </NavItem>
-                    </Nav>
                 <Form onSubmit={this.onSubmit}>
-                    <FormGroup className={this.getEditorMode() ? "invisible" : "visible"}>
-                        <textarea className="content-simple-text"
-                            defaultValue={this.state.content}
-                            onChange={this.onChange}/>
-                    </FormGroup>
-                    <FormGroup className={this.getEditorMode() ? "visible" : "invisible"}>
                         <MarkdownEditor id="content-markdown"
                             initialContent={this.state.content}
                             iconsSet="font-awesome"
                             onContentChange={this.onChange}/>
-                    </FormGroup>
-                    <FormGroup>
                         <Button className="comment-form-save" type="submit" disabled={this.state.disableButton}>
                             Comment
                         </Button>
-                    </FormGroup>
                 </Form>
             </div>
         );
