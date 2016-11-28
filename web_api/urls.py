ffbb5ada2dbac8d736186916ce643adcf142a2dd
Kevin Tang <tkevin@ualberta.ca>
2016-11-28 02:21:56 +0000

    can't deal with slashes

--- web_api/urls.py
+++ web_api/urls.py
@@ -17,36 +17,36 @@ urlpatterns = [
 
     # Specific post
     url(r'^', include(router.urls)),
-    url(r'^posts/$', views.PostView.as_view()),
-    url(r'^posts/(?P<pk>[^/.]+)/comments/$', views.CommentView.as_view()),
-    url(r'^posts/(?P<pk>[^/.]+)/$', views.PostIDView.as_view()),
-    url(r'^foreignposts/$', views.ForeignPostView.as_view()),
+    url(r'^posts/?$', views.PostView.as_view()),
+    url(r'^posts/(?P<pk>[^/.]+)/comments/?$', views.CommentView.as_view()),
+    url(r'^posts/(?P<pk>[^/.]+)/?$', views.PostIDView.as_view()),
+    url(r'^foreignposts/?$', views.ForeignPostView.as_view()),
 
     # Image
-    url(r'^images/$', views.ImageView.as_view()),
-    url(r'^images/(?P<pk>[^/.]+)/$', views.ImageIDView.as_view()),
+    url(r'^images/?$', views.ImageView.as_view()),
+    url(r'^images/(?P<pk>[^/.]+)/?$', views.ImageIDView.as_view()),
 
-    url(r'^author/$', views.AuthorViewSet.as_view()),
+    url(r'^author/?$', views.AuthorViewSet.as_view()),
     # Get all posts that author can see
-    url(r'^author/posts/$', views.AuthorStream.as_view()),
+    url(r'^author/posts/?$', views.AuthorStream.as_view()),
     
     # Images uploaded by currently authenticated author
-    url(r'^author/images/$', views.PersonalImagesView.as_view()),
+    url(r'^author/images/?$', views.PersonalImagesView.as_view()),
 
     # Used for updating author profile.
-    url(r'^author/(?P<pk>[^/.]+)/$', views.AuthorProfileUpdateView.as_view()),
+    url(r'^author/(?P<pk>[^/.]+)/?$', views.AuthorProfileUpdateView.as_view()),
     # Get all posts you can see from <authorID>
-    url(r'^author/(?P<pk>[^/.]+)/posts/$', views.PersonalAuthorStream.as_view()),
+    url(r'^author/(?P<pk>[^/.]+)/posts/?$', views.PersonalAuthorStream.as_view()),
     # Posts from specific Author
     # url(r'^author/(?P<pk>[^/.]+)/posts/$', views.SpecificPostView.as_view()),
 
     # Used for get the list of friends id or check the if an author is a friend of another author.
-    url(r'^friends/(?P<pk>[^/.]+)/$', views.FriendsWith.as_view()),
+    url(r'^friends/(?P<pk>[^/.]+)/?$', views.FriendsWith.as_view()),
     # Used for check 2 author is friend.
-    url(r'^friends/(?P<id1>[^/.]+)/(?P<id2>[^/.]+)/$', views.FriendCheck.as_view()),
-    url(r'^login/', views.Login.as_view()),
+    url(r'^friends/(?P<id1>[^/.]+)/(?P<id2>[^/.]+)/?$', views.FriendCheck.as_view()),
+    url(r'^login/?', views.Login.as_view()),
     # Used for sending friend requests.
-    url(r'^friendrequest/$', views.FriendRequestView.as_view()),
+    url(r'^friendrequest/?$', views.FriendRequestView.as_view()),
     
-    url(r'^friendsync/$', views.FriendSyncView.as_view()),
+    url(r'^friendsync/?$', views.FriendSyncView.as_view()),
 ]
