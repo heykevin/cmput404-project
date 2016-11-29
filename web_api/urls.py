7f49a8578deea5fcf2032b54cc8c75f00da64aa8
Kevin Tang <tkevin@ualberta.ca>
2016-11-29 01:17:01 +0000

    slash optional

--- web_api/urls.py
+++ web_api/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
 
     # Specific post
     url(r'^', include(router.urls)),
-    url(r'^posts/(?P<pk>[^/.]+)/$', views.PostIDView.as_view()),
+    url(r'^posts/(?P<pk>[^/.]+)/?$', views.PostIDView.as_view()),
     url(r'^posts/?$', views.PostView.as_view()),
     url(r'^posts/(?P<pk>[^/.]+)/comments/?$', views.CommentView.as_view()),
     url(r'^foreignposts/?$', views.ForeignPostView.as_view()),
