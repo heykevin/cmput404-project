02418a827bb60bfc0583aa40490e533ea05deedf
Kevin Tang <tkevin@ualberta.ca>
2016-11-29 10:30:21 +0000

    changing deploy to returnoftheblog

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Building API ------------------------------
-git remote add api https://git.heroku.com/api-bloggyblog404.git
+git remote add api https://git.heroku.com/api-returnoftheblog.git
 heroku buildpacks:set heroku/python --remote api
 git commit --allow-empty -m "Deploying api"
 git push --force api master
@@ -9,11 +9,11 @@ heroku run python manage.py migrate --remote api
 echo Building Client -----------------------------
 npm install
 npm run build-prod
-git remote add client https://git.heroku.com/bloggyblog404.git
+git remote add client https://git.heroku.com/returnoftheblog.git
 heroku buildpacks:set heroku/nodejs --remote client
 git add client
 git commit --allow-empty -m "Deploying Client"
 git push --force client master
 
-heroku ps:scale web=1 --app bloggyblog404
-heroku ps:scale web=1 --app api-bloggyblog404
+heroku ps:scale web=1 --app returnoftheblog
+heroku ps:scale web=1 --app api-returnoftheblog
